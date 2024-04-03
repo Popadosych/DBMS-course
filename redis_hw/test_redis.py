66d673d19e6a63f00c4ed6098aa02f3ab44c4752
Popadosych <sukhov.me@phystech.edu>
2024-04-04 02:42:14 +0300

    hw2 files added

---
+++ redis_hw/test_redis.py
@@ -0,0 +1,61 @@
+import redis
+import json
+import time
+
+client = redis.Redis()
+events = {}
+
+json_file = open('large-file.json', 'r')
+output = open('result.txt', 'w')
+events = json.load(json_file)
+
+
+
+
+print("------------SAVING------------", file=output)
+
+start_time = time.time()
+for i in range(len(events)):
+	client.set(i, str(events[i]))
+print("str", time.time() - start_time, file=output)
+
+start_time = time.time()
+for i in range(len(events)):
+	client.hset("hset_events", i, str(events[i]))
+print("hset", time.time() - start_time, file=output)
+
+start_time = time.time()
+for i in range(len(events)):
+	client.execute_command("ZADD", "zset_events", i, str(events[i]))
+print("zset", time.time() - start_time, file=output)
+
+start_time = time.time()
+for i in range(len(events)):
+	client.rpush("list_events", str(events[i]))
+print("list", time.time() - start_time, file=output)
+
+
+
+
+print("------------READING------------", file=output)
+
+start_time = time.time()
+client.get(5555)
+print("str", time.time() - start_time, file=output)
+
+start_time = time.time()
+client.hget("hset_events", 5555)
+print("hset", time.time() - start_time, file=output)
+
+start_time = time.time()
+client.zrange("zset_events", 5555, 5555)
+print("zset", time.time() - start_time, file=output)
+
+start_time = time.time()
+client.lrange("list_events", 5555, 5555)
+print("list", time.time() - start_time, file=output)
+
+
+
+json_file.close()
+output.close()
